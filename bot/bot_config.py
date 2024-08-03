b5ccab0bc8934d0f4cf3f33fff998cb5a7dc855e
Semyon <sonnenblumenglas@gmail.com>
2024-08-03 18:47:27 +0500

    level 2 bot

--- bot/bot_config.py
+++ bot/bot_config.py
@@ -1,5 +1,6 @@
 import logging
 import openai
+from openai import OpenAI
 from aiogram import Bot, Dispatcher
 from aiogram.fsm.context import FSMContext
 from aiogram.fsm.state import State, StatesGroup
@@ -7,6 +8,7 @@ from aiogram.fsm.storage.memory import MemoryStorage
 from aiogram.types import Message
 from aiogram.filters import Command
 from config import Config
+from openai.types.chat.completion_create_params import ResponseFormat as rs
 
 
 class MyStates(StatesGroup):
@@ -15,38 +17,45 @@ class MyStates(StatesGroup):
 
 class BotConfig:
     def __init__(self, config: Config):
+        self.context = None
         self.bot_token = config.get_bot_token()
-        self.openai_api_key = config.get_openai_api_key()
         self.bot = Bot(token=self.bot_token)
         self.storage = MemoryStorage()
         self.dp = Dispatcher(storage=self.storage)
         self.setup_handlers()
-
-        openai.api_key = self.openai_api_key
+        self.client = OpenAI(api_key=config.get_openai_api_key())
 
     def setup_handlers(self):
         self.dp.message.register(self.start, Command(commands=['start']))
         self.dp.message.register(self.handle_message)
 
+    def set_context(self, context: list[dict]):
+        self.context = context
+
     async def start(self, message: Message, state: FSMContext):
-        await message.answer("Привет! Я бот, готовый помочь. Напиши мне что-нибудь.")
+        await message.answer("Привет! Приглашаю тебя зарегистрироваться на наш хакатон: "
+                             "https://calendly.com/latoken-career-events/ai-hackathon.")
         await state.set_state(MyStates.WAITING_FOR_MESSAGE)
 
     async def handle_message(self, message: Message, state: FSMContext):
-        user_message = message.text
-        response = await self.get_openai_response(user_message)
+        response = await self.get_openai_response(message.text, message.from_user.first_name)
         await message.answer(response)
         await state.clear()
 
-    async def get_openai_response(self, user_message: str) -> str:
-        """Получение ответа от OpenAI Assistant."""
+    async def get_openai_response(self, message: str, username: str) -> str:
         try:
-            response = openai.Completion.create(
-                engine="text-davinci-003",  # Используйте модель, которая вам нужна
-                prompt=user_message,
-                max_tokens=150
+            response = self.client.chat.completions.create(
+                model="gpt-4o",
+                messages=self.context + [
+                    {"role": "user", "content": message},
+                ],
+                max_tokens=2500,
+                temperature=0.7,
+                frequency_penalty=1,
+                presence_penalty=0,
+                user=username
             )
-            return response.choices[0].text.strip()
+            return response.choices[0].message.content
         except Exception as e:
             logging.error(f"Error while calling OpenAI API: {e}")
             return "Произошла ошибка при обработке вашего запроса."
--- bot/main.py
+++ bot/main.py
@@ -1,16 +1,26 @@
 import asyncio
+import logging
+
 from config import Config
 from bot_config import BotConfig
-from aiogram import Bot, Dispatcher
+from resources import PROMPT, SYSTEM
+
+logging.basicConfig(level=logging.INFO)
 
 
 class LatokenAIBot:
     def __init__(self, _bot_config: BotConfig):
         self.config = bot_config
+        bot_config.set_context(self.create_context())
 
     async def run(self):
-        """Запуск бота."""
         await self.config.dp.start_polling(self.config.bot)
+
+    def create_context(self):
+        context = [{"role": "system", "content": SYSTEM},
+                   {"role": "system", "content": PROMPT}
+                   ]
+        return context
 
 
 if __name__ == '__main__':
@@ -19,5 +29,4 @@ if __name__ == '__main__':
     bot_config = BotConfig(config)
 
     bot = LatokenAIBot(bot_config)
-
     asyncio.run(bot.run())
